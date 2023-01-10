2461836023d9ab9b9e0fd078576f3f543974d6b0
Kaleb Zufanetti Santos <kalebzsantosk@gmail.com>
2023-01-10 17:45:08 -0300

    Update valida-cpf.js

--- js/valida-cpf.js
+++ js/valida-cpf.js
@@ -1,9 +1,9 @@
 export default function ehUmCPF(campo) {
     const cpf = campo.value.replace(/\.|-/g, "");
-    if(validaNumerosRepetidos(cpf) || validaPrimeirDigito(cpf) || validaSegundoDigito(cpf)) {
-        console.log("Esse cpf não existe!")
-    }else {
-        console.log("Existe!")
+    if (validaNumerosRepetidos(cpf) || validaPrimeiroDigito(cpf) || validaSegundoDigito(cpf)) {
+        console.log("Esse cpf não existe")
+    } else {
+        console.log("Esse cpf existe")
     }
 }
 
@@ -24,38 +24,38 @@ function validaNumerosRepetidos(cpf) {
     return numerosRepetidos.includes(cpf)
 }
 
-function validaPrimeirDigito(cpf) {
-    let soma = 0
-    let multiplicador = 10 
+function validaPrimeiroDigito(cpf) {
+    let soma = 0;
+    let multiplicador = 10;
 
-    for(let tamanho = 0; tamanho < 9; tamanho++) {
-        soma += cpf[tamanho] * multiplicador 
+    for (let tamanho = 0; tamanho < 9; tamanho++) {
+        soma += cpf[tamanho] * multiplicador;
         multiplicador--
     }
 
-    soma = (soma * 10) % 11
+    soma = (soma * 10) % 11;
 
-    if (soma == 10 || soma == 1) {
-        soma = 0
-
-        return soma != cpf[9]
+    if (soma == 10 || soma == 11) {
+        soma = 0;
     }
+
+    return soma != cpf[9];
 }
 
 function validaSegundoDigito(cpf) {
-    let soma = 0
-    let multiplicador = 11 
+    let soma = 0;
+    let multiplicador = 11;
 
-    for(let tamanho = 0; tamanho < 10; tamanho++) {
-        soma += cpf[tamanho] * multiplicador 
+    for (let tamanho = 0; tamanho < 10; tamanho++) {
+        soma += cpf[tamanho] * multiplicador;
         multiplicador--
     }
 
-    soma = (soma * 10) % 11
+    soma = (soma * 10) % 11;
 
-    if (soma == 10 || soma == 1) {
-        soma = 0
-
-        return soma != cpf[10]
+    if (soma == 10 || soma == 11) {
+        soma = 0;
     }
+
+    return soma != cpf[10];
 }
